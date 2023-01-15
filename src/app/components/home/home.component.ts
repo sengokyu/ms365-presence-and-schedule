73c2760ef316747430f14d110224f588dda99460
sengokyu <sengokyu+gh@gmail.com>
2023-01-15 22:54:34 +0900

    Force refresh session per a hour

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { interval, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private readonly subscription = new Subscription();
+
   editMode = false;
+
+  constructor(private oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit(): void {
+    this.subscription.add(
+      interval(3600 * 1000)
+        .pipe(switchMap(() => this.oidcSecurityService.forceRefreshSession()))
+        .subscribe((result) => {
+          console.debug('Session refreshed. result:' + result);
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }

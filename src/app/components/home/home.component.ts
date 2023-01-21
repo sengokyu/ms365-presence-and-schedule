09931a680fe00755322cddb9735fbec88b86fc03
sengokyu <sengokyu+gh@gmail.com>
2023-01-21 16:49:52 +0900

    Remove unnecesally refresh call.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,31 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { interval, Subscription } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  private readonly subscription = new Subscription();
-
+export class HomeComponent {
   editMode = false;
-
-  constructor(private oidcSecurityService: OidcSecurityService) {}
-
-  ngOnInit(): void {
-    this.subscription.add(
-      interval(3600 * 1000)
-        .pipe(switchMap(() => this.oidcSecurityService.forceRefreshSession()))
-        .subscribe((result) => {
-          console.debug('Session refreshed. result:' + result);
-        })
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
 }

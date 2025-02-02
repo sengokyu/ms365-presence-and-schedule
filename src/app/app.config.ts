c05fbb4dcff5f9ca1fe142f18eef98401eb355a7
sengokyu <sengokyu+gh@gmail.com>
2025-02-03 08:24:33 +0900

    Update app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { authInterceptor, provideAuth } from 'angular-auth-oidc-client';
+import { provideODataClient } from 'angular-odata';
+import { accessTokenInterceptor } from './access-token.interceptor';
 import { routes } from './app.routes';
 import { authConfig } from './auth/auth.config';
-import { provideODataClient } from 'angular-odata';
+import { providerMsGraphApi } from './ms-graph-api';
 import { odataConfig } from './odata/odata.config';
 
 export const appConfig: ApplicationConfig = {
@@ -13,8 +15,11 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAuth(authConfig),
-    provideHttpClient(withInterceptors([authInterceptor()])),
+    provideHttpClient(
+      withInterceptors([authInterceptor(), accessTokenInterceptor]),
+    ),
     provideAnimationsAsync(),
     provideODataClient(odataConfig),
+    providerMsGraphApi(),
   ],
 };

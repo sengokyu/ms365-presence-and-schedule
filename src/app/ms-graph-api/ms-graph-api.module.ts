54275ab8953c800ceb8815de7064f0ca29a1a1b1
sengokyu <sengokyu+gh@gmail.com>
2025-02-03 08:23:54 +0900

    Migrate ms-graph-api module

--- src/app/ms-graph-api/ms-graph-api.module.ts
+++ src/app/ms-graph-api/ms-graph-api.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
+import { makeEnvironmentProviders, NgModule } from '@angular/core';
 import { ODataModule } from 'angular-odata';
 import { msGraphApiConfig } from './ms-graph-api.config';
 import { UserService } from './services/user.service';
 import { UsersService } from './services/users.service';
 
+// Standalone version
+export const providerMsGraphApi = () =>
+  makeEnvironmentProviders([UserService, UsersService]);
+
+// Module version
 @NgModule({
   imports: [ODataModule.forRoot({ config: msGraphApiConfig })],
   exports: [ODataModule],
